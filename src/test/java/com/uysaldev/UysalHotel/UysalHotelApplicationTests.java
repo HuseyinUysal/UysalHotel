fc214457aa225a584c9278e96ab64faca6e41462
uber-clone <huseyin_uysal97@hotmail.com>
2025-02-14 12:41:45 +0300

    First commit

---
+++ src/test/java/com/uysaldev/UysalHotel/UysalHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uysaldev.UysalHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UysalHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
